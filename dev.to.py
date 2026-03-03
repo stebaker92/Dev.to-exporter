02ed5c539fe65f9b5b065141ee9268a9f6c8f781
Steve Baker <stebaker92@users.noreply.github.com>
2026-03-03 13:26:21 +0000

    Add fallbacks for unpublished posts

--- dev.to.py
+++ dev.to.py
@@ -14,12 +14,13 @@ response = requests.get('https://dev.to/api/articles/me/all',
 response.raise_for_status()
 
 articles = response.json()
-print("" + str(len(articles)) + ' articles found')
+print(str(len(articles)) + ' articles found')
 
 os.makedirs('posts', exist_ok=True)
 
 for post in articles:
-    parsed_date = post['published_at'].split('T')[0]
+    date_str = post.get('published_at') or post.get('created_at') or post.get('edited_at', 'unknown')
+    parsed_date = date_str.split('T')[0]
 
     # name markdown files with the following convention:
     # 2020-08-20-creating-a-portfolio-site-using-gatsby-trello.md
@@ -27,7 +28,7 @@ for post in articles:
 
     print('Creating file: ' + filename)
 
-    file = open('posts/' + filename + '.md', "w")
+    file = open('posts/' + filename + '.md', "w", encoding="utf-8")
     file.write(post['body_markdown'])
     file.close()
 
